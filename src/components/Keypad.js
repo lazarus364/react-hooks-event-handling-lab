0644f2f5fd937d033dbe5d6438109ef8cb70f21f
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2024-12-06 08:23:28 +0300

    key&eyes

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleChange = () => {
+        console.log('Entering password...');
+      };
+    
     return (
-        <div></div>
+        < input type="password" 
+        onChange={handleChange} 
+        placeholder="Enter your password"
+        />
     )
 }
 
-export default Keypad;
+export default Keypad
